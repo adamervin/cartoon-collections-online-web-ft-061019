24f45b3d9844130348d137bbfb33255da69cd2a3
Adam Ervin <adamjervin@gmail.com>
2019-06-22 00:42:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(smurfs)
+  smurfs.each.with_index do |name,index|
+    print "#{index + 1}#{name}"
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  my_array = array 
+  result = my_array.collect { |n| n.capitalize + "!"}
+   result
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  
+  
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
